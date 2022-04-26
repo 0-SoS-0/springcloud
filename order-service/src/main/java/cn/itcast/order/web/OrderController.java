1c7bb48ea1f52f617a95c4c99151dc73151c70e1
0-SoS-0 <15924630084@163.com>
2022-04-26 23:23:55 +0800

    nacos配ç配置管理的æ使用

--- order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -2,22 +2,42 @@ package cn.itcast.order.web;
 
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.service.OrderService;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("order")
-public class OrderController {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-   @Autowired
-   private OrderService orderService;
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+    @Autowired
+    private OrderService orderService;
+
+    @Value("${pattern.dateformat}")
+    private String dateFormat;
 
     @GetMapping("{orderId}")
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
         // 根据id查询订单并返回
         return orderService.queryOrderById(orderId);
+    }
+
+    @GetMapping("/now")
+    public String getNow() {
+        String nowTimeString = LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateFormat));
+
+        logger.info("当前日期 {}", nowTimeString);
+        return nowTimeString;
     }
 }
