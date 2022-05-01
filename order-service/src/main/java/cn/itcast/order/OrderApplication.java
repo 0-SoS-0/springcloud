53700dba0e5d38a045343298fe458c936e1c5877
0-SoS-0 <15924630084@163.com>
2022-05-01 23:32:11 +0800

    feign的不同实现方式

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,14 +1,14 @@
 package cn.itcast.order;
 
 import com.example.feignapi.feign.UserFeignClient;
-import com.example.feignapi.feign.config.DefaultFeignConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //开启基于feign功能的注解
-@EnableFeignClients(clients = {UserFeignClient.class})                 //解决FeignClient文件不在启动类下
+//@EnableFeignClients(clients = {UserFeignClient.class})                 //解决FeignClient文件不在启动类下
+@EnableFeignClients(basePackages ="com.example.feignapi.feign" )             //解决FeignClient文件不在启动类下
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
